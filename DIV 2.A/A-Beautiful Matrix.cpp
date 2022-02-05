7666b0105a6a830ef7b07ed245b1a63e67e177c7
MohitGupta121 <guptamm.22@gmail.com>
2022-02-05 14:49:37 +0530

    Beautiful Matrix

---
+++ DIV 2.A/A-Beautiful Matrix.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int a[6][6],r,c; // r is Row and c is Columns
+    for(int i=1; i<6; i++){
+        
+        for(int j=1; j<6; j++){
+            cin>>a[i][j];
+            
+            if(a[i][j]==1){
+                r = abs(3-i);
+                c = abs(3-j);
+                cout<< r+c;
+            }
+        }
+        
+    }
+}
