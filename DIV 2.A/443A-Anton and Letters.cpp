28c30b454c959ad6b3359233a672523eb8d93a34
MohitGupta121 <guptamm.22@gmail.com>
2022-02-09 23:46:34 +0530

    443A - Anton and Letters

---
+++ DIV 2.A/443A-Anton and Letters.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string s;
+    getline(cin,s);   // input whole string including space commas everything.
+    set<char> k;     // take unique char only
+    
+    for(int i=0; i<s.length(); i++){
+        if(isalpha(s[i]))  // only check is alphabets
+        {      
+            k.insert(s[i]);
+        }    
+        
+    }
+    
+    cout << k.size(); // print no. of char stores
+    
+}
