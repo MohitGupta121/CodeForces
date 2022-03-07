ad3612f05342dc593ea077c5a0087ed70d9bae7b
MOHIT GUPTA <76530270+MohitGupta121@users.noreply.github.com>
2022-03-08 00:01:28 +0530

    Update 152A-Marks.cpp

--- DIV 2.A/152A-Marks.cpp
+++ DIV 2.A/152A-Marks.cpp
@@ -1,28 +1,54 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    int n,m;
-    cin>>n>>m;
-    int a[n], b[m];
-    
-    for(int i=0; i<n; i++)
-        cin >> a[i];
-    for(int i=0; i<m; i++)
-        cin >> b[i];
-        
-    sort(a, a+n);
-    sort(b, b+m);
-    
-    int time = 2 * a[0];
-    
-    time = max(time, a[n-1]);
-    
-    if(b[0] <= time)
-        cout << -1;
-    else
-        cout << time;
-    
-    return 0;
+        int i,j,k;
+        int n,m;
+        int count=0;
+
+
+        cin>>n>>m;
+        char a[n][m],ch[n],max;
+        int s[n];
+
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                cin>>a[i][j];
+            }
+
+            s[i]=0;
+        }
+
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                ch[j]=a[j][i];
+            }
+
+            sort(ch,ch+n);
+            max=(ch[n-1]);
+
+            for(j=0;j<n;j++)
+            {
+                if(a[j][i]==max)
+                {
+
+                    s[j]++;
+                }
+            }
+
+        }
+
+        for(i=0;i<n;i++)
+        {
+            if(s[i]>0)
+                count++;
+        }
+
+        cout<<count<<endl;
+
+        return 0;
 }
