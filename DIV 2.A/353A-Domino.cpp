9945a39bfd58347ac268b4f74550a489b009bb71
MOHIT GUPTA <76530270+MohitGupta121@users.noreply.github.com>
2022-03-10 00:05:28 +0530

    Update 353A-Domino.cpp

--- DIV 2.A/353A-Domino.cpp
+++ DIV 2.A/353A-Domino.cpp
@@ -1,17 +1,24 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-int main()
-{
-    string s, t;
-    cin >> s >> t;
-    size_t pos = 0;
-    for (size_t i = 0; i < t.length(); ++i)
-    {
-        if (t[i] == s[pos])
-        {
-            pos += 1;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='U'){
+                s[i]='D';
+            }else if(s[i]=='D'){
+                s[i]='U';
+            }
         }
+        cout<<s<<"\n";
     }
-    cout << pos + 1 << endl;
     return 0;
 }
