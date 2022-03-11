a3e577d17d57bcc3dad9e5196fcd2a38efe4cb92
MOHIT GUPTA <76530270+MohitGupta121@users.noreply.github.com>
2022-03-11 23:25:13 +0530

    Update 353A-Domino.cpp

--- DIV 2.A/353A-Domino.cpp
+++ DIV 2.A/353A-Domino.cpp
@@ -2,23 +2,25 @@
 
 using namespace std;
 
-int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        string s;
-        cin>>s;
-
-        for(int i=0;i<n;i++){
-            if(s[i]=='U'){
-                s[i]='D';
-            }else if(s[i]=='D'){
-                s[i]='U';
-            }
+int main ()
+{
+    int n, l(0), r(0);
+    cin >> n;
+    bool s(false);
+    for (int x, y; cin >> x >> y;)
+    {
+        if (n == 1 and x % 2 + y % 2 == 1)
+        {
+            cout << -1;
+            return 0;
         }
-        cout<<s<<"\n";
+        if (y % 2 + x % 2 == 1)
+            s = true;
+        l += x;
+        r += y;
     }
+    if (l % 2 == 0 and r % 2 == 0) cout << 0 << endl;
+    else if (l % 2 == 1 and r % 2 == 1 and s) cout << 1 << endl;
+    else cout << -1 << endl;
     return 0;
 }
